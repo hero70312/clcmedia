b21606ef3000d4c496644264210c223fd73d6d53
HsiTingChou <hero70312@gmail.com>
2019-04-10 14:27:55 +0800

    first commit

---
+++ public/js/entry.js
@@ -0,0 +1,6 @@
+$(window).height();   // returns height of browser viewport
+console.log($(window).height());
+console.log($(document).height());
+console.log($(window).width());
+console.log($(document).width());
+console.log();
---
+++ text.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const app = express();
+app.use(bodyParser.json({limit: '100mb'}));
+
+// allow CORS
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization,locale");
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+    next();
+});
+
+// set public folder as site root
+app.use(express.static('public'));
+
+const port = process.env.PORT || 3007;
+
+app.listen(port, () => {
+    console.log(`listening on ${port}`);
+});
