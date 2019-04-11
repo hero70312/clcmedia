372a22f9d8cc5dc76ce0b104db7e4127e4c01b44
HsiTingChou <hero70312@gmail.com>
2019-04-11 14:07:14 +0800

    add click effect

--- public/js/entry.js
+++ public/js/entry.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
     $( "a" ).click(function() {
+
+        $(this).addClass( "animated" );
+
         $( this ).fadeOut( "fast",function(){
-            $( this ).fadeIn( "slow");
+            $(this).fadeIn( "slow");
+            $(this).removeClass( "animated" );
         });
     });
 });
