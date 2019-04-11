cd3462677373d63cd5736b3074c3a76045b1fc2a
HsiTingChou <hero70312@gmail.com>
2019-04-11 13:45:03 +0800

    頁面內轉換

--- public/js/entry.js
+++ public/js/entry.js
@@ -1,6 +1,5 @@
-$(window).height();   // returns height of browser viewport
-console.log($(window).height());
-console.log($(document).height());
-console.log($(window).width());
-console.log($(document).width());
-console.log();
+$(document).ready(function(){
+    $( "a" ).click(function() {
+        $( this ).fadeOut( "slow");
+    });
+});
